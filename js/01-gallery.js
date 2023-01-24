d7daddd37fa520adc983d976ff05e4c83d171f2f
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-01-24 17:24:04 +0100

    1/2 task complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+//
+const galleryEl = document.querySelector(".gallery");
+const imageCardsMarkup = createImageCardsMarkup(galleryItems);
 
-console.log(galleryItems);
+function createImageCardsMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+          <a class="gallery__link" href=${original}>
+            <img
+              class="gallery__image"
+              src=${preview}
+              data-source=${original}
+              alt=${description}
+            />
+          </a>
+        </div>`
+    )
+    .join("");
+}
+
+galleryEl.insertAdjacentHTML("beforeend", imageCardsMarkup);
+
+galleryEl.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  const dataSource = evt.target.getAttribute("data-source");
+  const description = evt.target.getAttribute("alt");
+  const instance = basicLightbox.create(
+    `<img src="${dataSource}" alt ="${description}">`
+  );
+
+  instance.show();
+
+  window.addEventListener("keydown", onEscKeyPress);
+
+  function onEscKeyPress(evt) {
+    if (instance.show && evt.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onEscKeyPress);
+    }
+  }
+}
