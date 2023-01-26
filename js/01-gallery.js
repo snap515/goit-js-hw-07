91e624b6e721fc63a59391d66dad01af1a90364f
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-01-26 17:40:33 +0100

    1/2 task update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,60 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 //
+
+// -------------------------- ПЕРВЫЙ СПОСОБ
+
+// const galleryEl = document.querySelector(".gallery");
+// const imageCardsMarkup = createImageCardsMarkup(galleryItems);
+
+// galleryEl.insertAdjacentHTML("beforeend", imageCardsMarkup);
+// galleryEl.addEventListener("click", onImageClick);
+
+// function createImageCardsMarkup(images) {
+//   return images
+//     .map(
+//       ({ preview, original, description }) =>
+//         `<div class="gallery__item">
+//           <a class="gallery__link" href=${original}>
+//             <img
+//               class="gallery__image"
+//               src=${preview}
+//               data-source=${original}
+//               alt=${description}
+//             />
+//           </a>
+//         </div>`
+//     )
+//     .join("");
+// }
+
+// function onImageClick(evt) {
+//   evt.preventDefault();
+
+//   if (evt.target.nodeName !== "IMG") {
+//     return;
+//   }
+
+//   const dataSource = evt.target.getAttribute("data-source");
+//   const description = evt.target.getAttribute("alt");
+//   const instance = basicLightbox.create(
+//     `<img src="${dataSource}" alt ="${description}">`
+//   );
+
+//   instance.show();
+
+//   document.addEventListener("keydown", onEscKeyPress);
+
+//   function onEscKeyPress(evt) {
+//     if (instance.show && evt.code === "Escape") {
+//       instance.close();
+//       window.removeEventListener("keydown", onEscKeyPress);
+//     }
+//   }
+// }
+
+// -------------------------- ВТОРОЙ СПОСОБ
+
 const galleryEl = document.querySelector(".gallery");
 const imageCardsMarkup = createImageCardsMarkup(galleryItems);
 
@@ -34,18 +88,25 @@ function onImageClick(evt) {
 
   const dataSource = evt.target.getAttribute("data-source");
   const description = evt.target.getAttribute("alt");
+
   const instance = basicLightbox.create(
-    `<img src="${dataSource}" alt ="${description}">`
+    `<img src="${dataSource}" alt ="${description}">`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: () => {
+        document.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
   );
 
-  instance.show();
-
-  window.addEventListener("keydown", onEscKeyPress);
-
   function onEscKeyPress(evt) {
-    if (instance.show && evt.code === "Escape") {
+    console.log(evt.code);
+    if (evt.code === "Escape") {
       instance.close();
-      window.removeEventListener("keydown", onEscKeyPress);
     }
   }
+
+  instance.show();
 }
